4cd18632507974b3dd43edfc59f3d56454443fb5
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-22 21:16:35 -0500

    Initial commit

--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,19 @@
 import {BeDecoratedProps, MinimalProxy, EventConfigs} from 'be-decorated/types';
+import {Target} from 'trans-render/lib/types';
 
 export interface EndUserProps {
     // when: string; //default:  beDecorated.indefinite.resolved;
     // passTo: string; //default:  beDecorated.indefinite.host
     // pullFrom: string; //default:  beDecorated.indefinite.clonedTemplate
+    hostTarget?: Target;
 }
 
 export interface VirtualProps extends EndUserProps, MinimalProxy{
+    propertyBag: EventTarget,
+    propertyBagUpdater: PropertyBagUpdater,
+}
+
+export interface PropertyBagUpdater{
 
 }
 
@@ -21,5 +28,5 @@ export type PPP = Partial<PP>;
 export type PPE = [PPP, EventConfigs<PPP, Actions>];
 
 export interface Actions{
-    
+
 }
